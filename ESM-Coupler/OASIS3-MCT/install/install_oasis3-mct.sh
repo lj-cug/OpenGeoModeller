bfb9a5f5bb1318c0a10cd33c060e2ee9522203c9
lj-cug <53997605+lj-cug@users.noreply.github.com>
2024-04-06 15:30:24 +0800

    update

--- ESM-Coupler/OASIS3-MCT/install/install_oasis3-mct.sh
+++ ESM-Coupler/OASIS3-MCT/install/install_oasis3-mct.sh
@@ -2,8 +2,8 @@
 cd oasis3-mct/util/make_dir
 ## Adapt the "make.inc" file to include your platform header makefile
 ## Adapt the value of $COUPLE and $ARCHDIR in your platform header makefile
-make realclean ¨Cf TopMakefileOasis3
-make ¨Cf TopMakefileOasis3
+make realclean -f TopMakefileOasis3
+make -f TopMakefileOasis3
 ## The libraries "libmct.a", "libmpeu.a", "libpsmile.MPI1.a" and "libscrip.a" that need to
 ##   be linked to the models are available in the directory $ARCHDIR/lib
 
